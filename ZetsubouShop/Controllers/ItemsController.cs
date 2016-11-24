f470b37fde5113e1aa5f06bc6f194ce96d3426d8
Turlei Roman <r.turlei.rt@gmail.com>
2016-11-24 14:53:04 +0300

    Added web project.

---
+++ ZetsubouShop/Controllers/ItemsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ZetsubouShop.Models;
+
+namespace ZetsubouShop.Controllers
+{
+    //[Authorize]
+    public class ItemsController : ApiController
+    {
+        private readonly ApplicationDbContext _db = new ApplicationDbContext();
+        // GET api/values
+        public IEnumerable<Item> Get()
+        {
+            return _db.Items;
+        }
+
+        // GET api/values/5
+        public Item Get(Guid id)
+        {
+            return _db.Items.FirstOrDefault(a => a.Id == id);
+        }
+
+        // POST api/values
+        public void Post([FromBody]Item item)
+        {
+            item.Id = Guid.NewGuid();
+            _db.Items.Add(item);
+            _db.SaveChanges();
+        }
+
+        // PUT api/values/5
+        public void Put(Guid id, [FromBody]Item item)
+        {
+            if (id == item.Id)
+            {
+                _db.Entry(item).State = EntityState.Modified;
+
+                _db.SaveChanges();
+            }
+        }
+
+        // DELETE api/values/5
+        public void Delete(Guid id)
+        {
+            var item = _db.Items.FirstOrDefault(a => a.Id == id);
+            if (item != null)
+            {
+                _db.Items.Remove(item);
+                _db.SaveChanges();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ZetsubouShopWeb/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZetsubouShopWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        HttpClient client;
+        string url = "http://localhost:3077/api/";
+        HomeController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+           
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
